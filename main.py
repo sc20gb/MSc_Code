00dd3840f56d7b7d5d56f1cb930c65dc425a197c
sc20gb <77701666+sc20gb@users.noreply.github.com>
2024-07-08 18:47:29 +0100

    Added code to check for cuda

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import torchvision.transforms as transforms
 import os
 from data_loading import load_data,  display_sample
+import torch
+
 
 BATCHSIZE  = 32
 
@@ -14,6 +16,18 @@ test_loader, train_loader, validate_loader = load_data(transforms.Compose([
     transforms.ToTensor()
 ]), BATCHSIZE, RANDSEED, os.path.join(os.getcwd(), 'Slake1.0')
 )
+
+# Check if CUDA (GPU support) is available
+if torch.cuda.is_available():
+    # Get the number of GPUs available
+    gpu_count = torch.cuda.device_count()
+    print(f"CUDA is available with {gpu_count} GPU(s)!")
+    # Print the name of each GPU available
+    for i in range(gpu_count):
+        print(f"GPU {i}: {torch.cuda.get_device_name(i)}")
+else:
+    print("CUDA is not available. Training will proceed on CPU.")
+
 
 for images, masks, questions, answers in train_loader:
     # Visualize the first image in the batch
