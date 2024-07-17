c097aa6342e34af86036f84a5bbe6caf4dfc7953
sc20gb <77701666+sc20gb@users.noreply.github.com>
2024-07-17 16:20:56 +0100

    Working Untrained GPT2 Model

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import torchvision.transforms as transforms
 import os
 from data_loading import load_data,  display_sample
 #from TextTransformerEncoder import TextTransformerEncoder
-from vicuna_llm import CustomLlamaModel
 import torch
+import transformers
+
+import torch.nn as nn
 
 # import warnings
 # warnings.filterwarnings("ignore")
@@ -16,10 +18,12 @@ BATCHSIZE  = 32
 
 RANDSEED  = 42
 
+MAX_LENGTH =  76
+
 IMAGESIZE = 240
 
 
-# Check GPU support
+# CHECK GPU SUPPORT AND ASSIGN DEVICE
 if torch.cuda.is_available():
     # Get the number of GPUs available
     gpu_count = torch.cuda.device_count()
@@ -35,7 +39,6 @@ else:
     print("CUDA is not available. Training will proceed on CPU.")
     device = torch.device("cpu")
 
-
 #LOAD DATA
 test_loader, train_loader, validate_loader = load_data(transforms.Compose([
     transforms.Resize((IMAGESIZE, IMAGESIZE)),
@@ -43,22 +46,81 @@ test_loader, train_loader, validate_loader = load_data(transforms.Compose([
 ]), BATCHSIZE, RANDSEED, os.path.join(os.getcwd(), 'Slake1.0')
 )
 
-#TESTING CLIP_ViT
+# CONTRASTIVE LANGUAGE-IMAGE PRE-TRAINING
 
+# Load the ViT
 from clip_ViT import ClipEncoder
+imageEncoder = ClipEncoder(d_model=512,n_heads=8,r_mlp=4, img_size=IMAGESIZE, patch_size=30, n_channels=3,max_seq_length=256)
 
-imageEncoder = ClipEncoder(d_model=512,n_heads=8,r_mlp=4, img_size=240, patch_size=30, n_channels=3,max_seq_length=256)
+# Load GPT2 Model (text-encoder)
+from transformers import GPT2Tokenizer,  GPT2Model, GPT2Config, GPT2LMHeadModel
 
-for image_tensor, mask_tensor, question_tensor, answer_tensor in train_loader:
+# Change the config to that described by the CLIP paper
+config = GPT2Config.from_json_file(os.path.join(os.getcwd(), "gpt2Config","config.json"))
+
+# The Clip paper does not create a new tokenizer but uses one with a 49,152 vocab size
+textEncoderTokeniser = GPT2Tokenizer.from_pretrained('gpt2')
+textEncoderTokeniser.pad_token = textEncoderTokeniser.eos_token
+
+#Load the head as well
+LMHead = GPT2LMHeadModel(GPT2Config.from_json_file(os.path.join(os.getcwd(), "gpt2Config","config.json")))
+
+# Initalise an untrained model 
+textEncoder = GPT2Model(config)
+
+# Training
+
+for image_tensor, mask_tensor, question, answer in train_loader:
+
+    #Encode Image
     imgEncodings = imageEncoder(image_tensor)
     print("Image Encoded to ", imgEncodings.size())
+
+    #Tokenize question and answer
+    text = [a + " " + b for a, b in zip(question,answer)]
+
+    tokens = textEncoderTokeniser(text, padding=True, truncation=True, return_tensors='pt', max_length=MAX_LENGTH)
+
+    print(type(tokens))
+
+    #Pass to the textEncoder,  Includes the mask
+    #EncoderOut = textEncoder(**tokens)
+    DecoderOut = LMHead(**tokens)
+
+    print(DecoderOut.logits.size())
     break
 
-# TESTING TEXT ENCODER
+    #  TODO:  Work out how to Train the two
+        # Could be that we train the llm on the question and answer pairs?
+    # TODO: Make sure that the  GPT2 model is the  right model, config and all
+    # TODO: Make sure taht the tokenizer is correct
 
 
 
-# 
+
+
+
+
+
+
+
+# Take the two embeddings and preject them linearly to a new joint embedding space
+
+
+
+# for m in textEncoder.modules():
+#     if not isinstance(m, nn.Sequential):
+#         print(m)
+
+
+textEncoder = torch.hub.load('huggingface/transformers', 'modelForCausalLM', 'gpt2')
+
+# load empty tokenizer for gpt2 model
+tokenizer = torch.hub.load('huggingface/transformers', 'tokenizer', 'gpt2')
+
+
+string = "Hello?"
+
 
 
 
