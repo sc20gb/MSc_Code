1ba6d92b70b1ed871c810fdab61c2315fd912a96
sc20gb <77701666+sc20gb@users.noreply.github.com>
2024-08-29 19:49:58 +0100

    Create job_script_finetuning_step_1.sh

---
+++ job_script_finetuning_step_1.sh
@@ -0,0 +1,18 @@
+#Python single core submission script
+
+#Run with current environment (-V) and in the current directory (-cwd)
+#$ -V -cwd
+
+#Request some time- min 15 mins - max 48 hours
+#$ -l h_rt=03:00:00
+
+#Request some memory per core
+#$ -l h_vmem=100G
+
+#$ -l coproc_v100=1
+
+#Get email at start and end of the job
+#$ -m be
+
+#Now run the job
+python fine_tuning.py
