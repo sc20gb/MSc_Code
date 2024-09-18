3ed1f0ec61b8698f5fb9f36e414118de087c24c0
sc20gb <77701666+sc20gb@users.noreply.github.com>
2024-09-18 10:52:02 +0100

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,15 @@
+#Python single core submission script
+
+#Run with current environment (-V) and in the current directory (-cwd)
+#$ -V -cwd
+
+#Request some time- min 15 mins - max 48 hours
+#$ -l h_rt=01:00:00
+
+#$ -l coproc_v100=1
+
+#Get email at start and end of the job
+#$ -m be
+
+#Now run the job
+python test.py
