93b491a85e9e8283661be33f5f6dcd58a75923cc
sc20gb <77701666+sc20gb@users.noreply.github.com>
2024-09-23 21:32:06 +0100

    Update fine_tune.py

--- fine_tune.py
+++ fine_tune.py
@@ -54,8 +54,8 @@ def calc_loss_and_metrics(predicted, target, tokenizer):
         predicted_item = predicted[i]
         
         # Ensure the answer has its capitals and whitespace removed
-        predicted_string = process_string(tokenizer.decode(predicted_item.int(), skip_special_tokens=True))
-        target_string = process_string(tokenizer.decode(target_item.int(), skip_special_tokens=True))
+        predicted_string = process_string(tokenizer.decode(predicted_item.long(), skip_special_tokens=True))
+        target_string = process_string(tokenizer.decode(target_item.long(), skip_special_tokens=True))
 
         print(predicted_string)
         print(target_string)
