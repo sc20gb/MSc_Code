Fix regularisation loss calculation in Connector_LLM_With_Gen_Reg class to use image embeddings

src/Model_Defs/connector_LLM_gen_reg.py
@@ -220,7 +220,7 @@ class Connector_LLM_With_Gen_Reg(nn.Module):
             #TODO:Normalise the embeddings before the cosine simularity loss, to ensure the loss is not dominated by the image embeddings
             # projected_norm = F.normalize(projected_img_embeddings, p=2, dim=2)
             # reconstructed_norm = F.normalize(reconstructed_image_embeddings, p=2, dim=2)
-            regularisation_loss = 1.0 - F.cosine_similarity(projected_img_embeddings, reconstructed_image_embeddings, dim=2).mean()
+            regularisation_loss = 1.0 - F.cosine_similarity(image_embeddings, reconstructed_image_embeddings, dim=2).mean()
 
         # construct the final loss
         loss = token_prediction_loss + self.lamda * regularisation_loss